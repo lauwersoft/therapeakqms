5d393be7c2d9dde72a4e759ac111ece3334bc095
Sarp <sarp@piggy.nl>
2026-03-23 01:01:27 +0100

    WIP

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -92,7 +92,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden absolute left-0 right-0 bg-white shadow-lg border-b border-gray-200">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
