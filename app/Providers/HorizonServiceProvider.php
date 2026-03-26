4facd348b25ab8bcd4fb7146474763c923c6627e
Sarp <sarp@piggy.nl>
2026-03-26 17:25:30 +0100

    WIP

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    public function boot(): void
+    {
+        parent::boot();
+
+        Horizon::night();
+    }
+
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function ($user = null) {
+            return $user && $user->role === User::ROLE_ADMIN;
+        });
+    }
+}
