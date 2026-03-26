30b10c8b09b8943bde7176a74be2696842b088ce
Sarp <sarp@piggy.nl>
2026-03-26 02:57:13 +0100

    WIP

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-3 lg:-my-px lg:ms-6 lg:flex text-sm">
+                <div class="hidden space-x-6 min-[1150px]:-my-px min-[1150px]:ms-10 min-[1150px]:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
@@ -46,7 +46,7 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden lg:flex lg:items-center lg:ms-4">
+            <div class="hidden min-[1150px]:flex min-[1150px]:items-center min-[1150px]:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -86,7 +86,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center lg:hidden">
+            <div class="-me-2 flex items-center min-[1150px]:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -105,12 +105,12 @@
          x-transition:leave="transition-opacity ease-in duration-100"
          x-transition:leave-start="opacity-100"
          x-transition:leave-end="opacity-0"
-         @click="open = false" class="fixed left-0 right-0 bottom-0 bg-gray-900/50 lg:hidden" style="z-index: 45; top: 4rem; display: none;" x-cloak></div>
+         @click="open = false" class="fixed left-0 right-0 bottom-0 bg-gray-900/50 min-[1150px]:hidden" style="z-index: 45; top: 4rem; display: none;" x-cloak></div>
 
     <!-- Responsive Navigation Menu -->
     <div x-show="open" x-transition:enter="transition ease-out duration-150" x-transition:enter-start="opacity-0 -translate-y-2" x-transition:enter-end="opacity-100 translate-y-0"
          x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 -translate-y-2"
-         class="lg:hidden absolute left-0 right-0 bg-white shadow-lg border-b border-gray-200" style="z-index: 55;" x-cloak>
+         class="min-[1150px]:hidden absolute left-0 right-0 bg-white shadow-lg border-b border-gray-200" style="z-index: 55;" x-cloak>
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
