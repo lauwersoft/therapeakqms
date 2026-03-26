c336eee94594e19fb92a09d22f0000d014a9f3d6
Sarp <sarp@piggy.nl>
2026-03-26 18:51:42 +0100

    WIP

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -141,6 +141,9 @@
                                 </div>
                             </div>
                             <div class="flex items-center gap-1 shrink-0">
+                                <a href="{{ route('activity.show', $user) }}" class="p-1.5 rounded hover:bg-gray-100 text-gray-400 hover:text-gray-600" title="Activity">
+                                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/></svg>
+                                </a>
                                 <a href="{{ route('users.edit', $user) }}" class="p-1.5 rounded hover:bg-gray-100 text-gray-400 hover:text-gray-600" title="Edit">
                                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/></svg>
                                 </a>
