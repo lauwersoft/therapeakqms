fe2de5c15064caecbb4f3f6a1c916e70a63fbcd8
Sarp <sarp@piggy.nl>
2026-03-27 01:57:31 +0100

    WIP

--- resources/views/documents/browse.blade.php
+++ resources/views/documents/browse.blade.php
@@ -248,7 +248,7 @@
             </div>
         @endif
 
-        <div class="flex flex-col h-full overflow-y-auto">
+        <div class="flex flex-col h-full overflow-y-auto bg-gray-100">
         <div class="max-w-5xl w-full mx-auto px-4 sm:px-6 lg:px-8 pt-6">
             {{-- Unpublished changes banner --}}
             @if($pendingCount > 0 && $canEdit)
